188beec05ce1c933ffb4f63f09ccbe8b3801416b
Alexandre <juniorbrasolin3@gmail.com>
2026-06-15 18:00:34 -0300

    atualizações

---
+++ eicamargo/js/addimagem.js
@@ -0,0 +1,16 @@
+const upload = document.getElementById("upload-imagem");
+const preview = document.getElementById("preview-imagem");
+
+upload.addEventListener("change", function(){
+
+    const arquivo = this.files[0];
+
+    if(arquivo){
+
+        preview.src = URL.createObjectURL(arquivo);
+
+        preview.style.display = "block";
+
+    }
+
+});
