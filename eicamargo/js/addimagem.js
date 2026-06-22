cd8e5774328ebbd17893c77aa446362b5c9c48d8
unknown <nanat16.0408@gmail.com>
2026-06-22 08:06:52 -0300

    Comentário inutéis tirados

--- eicamargo/js/addimagem.js
+++ eicamargo/js/addimagem.js
@@ -2,15 +2,9 @@ const upload = document.getElementById("upload-imagem");
 const preview = document.getElementById("preview-imagem");
 
 upload.addEventListener("change", function(){
-
     const arquivo = this.files[0];
-
     if(arquivo){
-
         preview.src = URL.createObjectURL(arquivo);
-
         preview.style.display = "block";
-
     }
-
 });
